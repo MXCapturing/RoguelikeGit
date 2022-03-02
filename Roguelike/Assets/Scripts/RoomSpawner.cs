d46ca408cf8eb087be0d138d83210247c9f095dd
Unknown <MXCapturing@gmail.com>
2022-03-03 00:55:47 +1300

    Starting Random Dungeon Generation

---
+++ Roguelike/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+    public int openingDir_;
+    //if 1 -> Needs Bottom Door
+    //if 2 -> Needs Top Door
+    //if 3 -> Needs Left Door
+    //if 4 -> Needs Right Door
+
+    private void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        if (!spawned)
+        {
+            if (openingDir_ == 1)
+            {
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+            else if (openingDir_ == 2)
+            {
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDir_ == 3)
+            {
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDir_ == 4)
+            {
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+            spawned = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
